3e3077b6eea035ede04151e1fe31222316e7b0d6
Manuel Ibáñez Hacha <bymanuxxyt@hotmail.com>
2025-12-25 18:57:21 +0100

    Added icons, made home and header responsive

--- src/app/Components/header/header.ts
+++ src/app/Components/header/header.ts
@@ -1,12 +1,20 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+
+  //Declaramos nuestro booleano en false, para cambiar según se clicka en el icono del menú hamburguesa
+  isMenuOpen: Boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
